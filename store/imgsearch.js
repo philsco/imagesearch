dd0094f677a15b8e8241c5d5951de246f33f1022
philsco <philsco@cloudshiftit.com>
2016-01-29 20:07:02 -0800

    Modified DB connectivity check to be part of the export module and use the callback

--- store/imgsearch.js
+++ store/imgsearch.js
@@ -18,43 +18,40 @@ var PixaReq = function (queryObj) {
     this.image_type = "photo";
 }
 
-if (mongoose.connection.readyState !== 1) {
-    mongoose.connect(urlString, function (err, res) {
-        if (err) {
-            console.log('err on connect');
-            callback({"error":"Connection", "message":"Experiencing database connecivity issues, please try again"});
-        } else {
-            console.log('successfully connected')                
-            }
-        });
-    };
-    
-    
 module.exports = function (action, payload, callback) {
-    if (action === "saveQuery") {
-    var pixareq = new PixaReq(payload); 
-    var Schq = mongoose.model('SearchQs', schSchema);
-    var newQuery = new Schq();
-    newQuery.schdate = new Date();
-    newQuery.schquery = pixareq.q.join(" ");
-    newQuery.save(function (err) {
-        if (err) {
-            callback(JSON.stringify({"error": "save", "message": "Error on saving the query. Please try again."}));
-        } else {
-            var pixareq = new PixaReq(payload); 
-            var pixaUrl = "https://pixabay.com/api?key="
-                        +encodeURIComponent(process.env.PIXABAY_KEY)
-                        +"&q="+encodeURIComponent(pixareq.q.join("+").toString())
-                        +"&image_type=photo&per_page=10&page="
-                        +encodeURIComponent(pixareq.p);
-            request.get(pixaUrl, (err, resp, body) => {
-            if (!err && resp.statusCode == 200) {
-                callback(JSON.stringify(body));
+    if (mongoose.connection.readyState !== 1) {
+        mongoose.connect(urlString, function (err, res) {
+            if (err) {
+                console.log('err on connect');
+                callback(JSON.stringify({"error":"Connection", "message":"Experiencing database connecivity issues, please try again"}));
             } else {
-                callback(JSON.stringify({"error":"api", "message":"Experienced an error with the image search API"}));
+                console.log('successfully connected')                
                 }
             });
-        }
+    } else if (action === "saveQuery") {
+        var pixareq = new PixaReq(payload); 
+        var Schq = mongoose.model('SearchQs', schSchema);
+        var newQuery = new Schq();
+        newQuery.schdate = new Date();
+        newQuery.schquery = pixareq.q.join(" ");
+        newQuery.save(function (err) {
+            if (err) {
+                callback(JSON.stringify({"error": "save", "message": "Error on saving the query. Please try again."}));
+            } else {
+                var pixareq = new PixaReq(payload); 
+                var pixaUrl = "https://pixabay.com/api?key="
+                            +encodeURIComponent(process.env.PIXABAY_KEY)
+                            +"&q="+encodeURIComponent(pixareq.q.join("+").toString())
+                            +"&image_type=photo&per_page=10&page="
+                            +encodeURIComponent(pixareq.p);
+                request.get(pixaUrl, (err, resp, body) => {
+                if (!err && resp.statusCode == 200) {
+                    callback(JSON.stringify(body));
+                } else {
+                    callback(JSON.stringify({"error":"api", "message":"Experienced an error with the image search API"}));
+                    }
+                });
+            }
         });   
     } else {
       var Schq = mongoose.model('SearchQs', schSchema);
